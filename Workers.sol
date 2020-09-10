c8d5d7e967c27fc310eea46590efd7e37554f298
jonsax6 <jdc.saxjunky@gmail.com>
2020-09-10 12:37:35 -0400

    Update Workers.sol
    -removed deletePerson function reference from fireWorker function.  replaced with delete people[terminated]

--- Workers.sol
+++ Workers.sol
@@ -6,10 +6,9 @@ import "./People.sol";
 pragma solidity 0.5.12;
 
 contract Workers is Ownable, People{
-    // Should extend the People contract by adding another mapping called salary which maps an address to an integer. 
+    // add mapping called salary which maps an address to an integer. 
     mapping(address => uint) private annSalary;
-    // For each worker created, you need to input and save information about who (which address) is the boss. 
-    // This should be implemented in the Worker contract.
+    // For the address of each worker created, map the address of the employer
     mapping(address => address) private employer;
     
     event PersonFired(string name, uint age, address FiredBy);
@@ -20,18 +19,18 @@ contract Workers is Ownable, People{
         _;
     }
 
-    // Have a createWorker function which is a wrapper function for the createPerson function. Make sure to figure out 
-    // the correct visibility level for the createPerson function (it should no longer be public). This is the only modification 
-    // we should do in the People contract.
+    // createWorker function which is a wrapper function for the createPerson function.   
+    // make the correct visibility level for the createPerson function (internal). 
     function createWorker(string memory name, uint age, uint height, uint salary, address boss) public payable costs(100 wei){
-        // When creating a worker, the persons age should not be allowed to be over 75. 
+        // persons age should not be allowed to be over 75. 
         require(age <= 75);
-        // A worker is not allowed to be his/her own boss. 
+        // worker is not allowed to be his/her own boss. 
         require(msg.sender != boss);
+        //reference createPerson function in People contract
         createPerson(name, age, height);
-        // Apart from calling the createPerson function, the createWorker function should also set the salary for the Worker in the 
-        // new mapping.
+        // set the salary for the Worker in the new mapping.
         annSalary[msg.sender] = salary;
+        // For the address of each worker created, map the address of the employer
         employer[msg.sender] = boss;
     }
     // Implement a fire function, which removes the worker from the contract.
@@ -40,7 +39,7 @@ contract Workers is Ownable, People{
         require(msg.sender == employer[terminated]);
         string memory name = people[terminated].name;
         uint age = people[terminated].age;
-        deletePerson(terminated);
+        delete people[terminated];
         // By implementing a new function in the base contract, used by both deletePerson and fire, make sure there is as little code 
         // duplication as possible between the deletePerson function and the fire function. 
         verifyRemoved(name, age, terminated);
