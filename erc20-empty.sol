d9c2990c76343276baababad3976be8646b2d0d6
jonsax6 <jdc.saxjunky@gmail.com>
2020-09-11 19:28:52 -0400

    Update erc20-empty.sol

--- erc20-empty.sol
+++ erc20-empty.sol
@@ -18,29 +18,39 @@ contract ERC20 {
 
 
     function name() public view returns (string memory) {
-
+        return _name;
     }
 
     function symbol() public view returns (string memory) {
-
+        return _symbol;
     }
 
     function decimals() public view returns (uint8) {
-
+        return _decimals;
     }
 
     function totalSupply() public view returns (uint256) {
-
+        return _totalSupply;
 
     function balanceOf(address account) public view returns (uint256) {
-
+        return _balances[account];
     }
 
     function mint(address account, uint256 amount) public {
+        require(account != address(0), "mint to the zero address");
 
+        _totalSupply = _totalSupply + amount;
+        _balances[account] = _balances[account] + amount;
     }
 
     function transfer(address recipient, uint256 amount) public returns (bool) {
+        require(msg.sender != address(0), "transfer from the zero address");
+        require(recipient !=  address(0), "transfer to the zero address");
+        require(_balances[msg.sender] >= amount, "Insufficient balance");
 
+        _balances[msg.sender] = _balances[msg.sender] - amount;
+        _balances[recipient] = _balances[recipient] + amount;
+
+        return true;
     }
 }
